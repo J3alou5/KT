e9e5872b995682f414efc592e153ae172d28f49d
J3alou5 <karliskade@gmail.com>
2024-04-05 19:44:27 +0300

    new

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,19 @@
-import logo from '../assets/logo.jpg'
+// Header.js
+import React from 'react';
+import logo from '../assets/logo.jpg'; // Adjust the import path to where your actual logo is stored
 
 const Header = () => {
     return (
         <header id="main-header">
             <div id="title">
-                <img src={logo}/>
+                <img src={logo} alt="Food Order App Logo"/>
                 <h1>React Food Order App</h1>
             </div>
             <nav>
-            <button>Cart (0)</button>
+                <button>Cart (0)</button>
             </nav>
         </header>
-    )
-}
+    );
+};
 
-export default Header
+export default Header;
